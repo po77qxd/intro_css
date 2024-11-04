d6ea24315f87c81a5adcb513b5b8e668b47a7db8
Bastien <po77qxd@eduvaud.ch>
2024-11-04 15:15:25 +0100

    add two element to modify(par2 and footer)

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,16 @@
 function applyCss(){
-    var resultArea = document.getElementById("resultArea");
     var titre = document.getElementById("titre");
-    var par1 = document-getElementById("paragraphe1");
+    var par1 = document.getElementById("paragraphe1");
+    var par2 = document.getElementById("paragraphe2");
+    var footer = document.getElementById("footer");
 
     var cssTitle = document.getElementById("cssTitre").value;
     var cssPar1 = document.getElementById("cssPar1").value;
+    var cssPar2 = document.getElementById("cssPar2").value;
+    var cssFooter = document.getElementById("cssFooter").value;
 
     titre.setAttribute("style", cssTitle);
     par1.setAttribute("style", cssPar1);
-    //resultArea.setAttribute("style", "border-color: #FF0000; color: #00FF000;");
+    cssPar2.setAttribute("style", cssPar2);
+    cssFooter.setAttribute("style", cssFooter);
 }
