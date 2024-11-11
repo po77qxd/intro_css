fecf63fafbf6020c3261a6b82564fea11934850f
Bastien <po77qxd@eduvaud.ch>
2024-11-11 14:00:15 +0100

    add bootstrap

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,16 @@
 function applyCss(){
     var titre = document.getElementById("titre");
     var par1 = document.getElementById("paragraphe1");
-    var par2 = document.getElementById("paragraphe2");
+    var resultArea = document.getElementById("resultArea");
     var footer = document.getElementById("footer");
 
     var cssTitle = document.getElementById("cssTitre").value;
     var cssPar1 = document.getElementById("cssPar1").value;
-    var cssPar2 = document.getElementById("cssPar2").value;
+    var cssResultArea = document.getElementById("cssResultArea").value;
     var cssFooter = document.getElementById("cssFooter").value;
 
     titre.setAttribute("style", cssTitle);
     par1.setAttribute("style", cssPar1);
-    par2.setAttribute("style", cssPar2);
+    resultArea.setAttribute("style", cssResultArea);
     footer.setAttribute("style", cssFooter);
 }
