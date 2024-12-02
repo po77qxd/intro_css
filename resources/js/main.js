10798198ca4212b0b787f6393e5da4069704c986
Bastien <po77qxd@eduvaud.ch>
2024-12-02 13:05:11 +0100

    add an image in html to modify

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,22 +2,25 @@ function applyCss(){
     var titre = document.getElementById("titre");
     var par1 = document.getElementById("paragraphe1");
     var resultArea = document.getElementById("resultArea");
+    var img = document.getElementById("img");
     var footer = document.getElementById("footer");
 
     var cssTitle = document.getElementById("cssTitre").value;
     var cssPar1 = document.getElementById("cssPar1").value;
     var cssResultArea = document.getElementById("cssResultArea").value;
+    var cssImg = document.getElementById("cssImg").value;
     var cssFooter = document.getElementById("cssFooter").value;
 
     titre.setAttribute("style", cssTitle);
     par1.setAttribute("style", cssPar1);
     resultArea.setAttribute("style", cssResultArea);
+    img.setAttribute("style", cssImg);
     footer.setAttribute("style", cssFooter);
 }
 
 function openHelp(){
     var helpCard = document.getElementById("helpCard");
-    helpCard.setAttribute("style", "display: block; width: 100%; top: -108%; left: -20px;" );
+    helpCard.setAttribute("style", "display: block; width: 100%; top: -100%; left: -20px;" );
     var todoArea = document.getElementById("todoArea");
     todoArea.setAttribute("style", "height: 700px;" );
 }
@@ -29,7 +32,7 @@ function closeHelp(){
 
 function openAnswer(){
     var answerCard = document.getElementById("answerCard");
-    answerCard.setAttribute("style", "display: block; width: 100%; top: -108%; left: -20px;" );
+    answerCard.setAttribute("style", "display: block; width: 100%; top: -100%; left: -20px;" );
     var todoArea = document.getElementById("todoArea");
     todoArea.setAttribute("style", "height: 700px;" );
 }
